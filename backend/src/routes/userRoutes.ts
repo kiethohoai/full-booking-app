ffabe33ac16ead2cdce39813250147ae5b1ae23b
kiethohoai <kietho.060193@gmail.com>
2024-09-27 20:51:51 +0700

    #8 Register Validation

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,45 +1,62 @@
 import express, { Request, Response } from 'express';
 import User from '../models/userModel';
 import jwt from 'jsonwebtoken';
+import { check, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('/register', async (req: Request, res: Response): Promise<any> => {
-  try {
-    // Check User Exist
-    let user = await User.findOne({ email: req.body.email });
-    if (user) {
-      return res.status(400).json({ message: 'User Already Exist' });
+router.post(
+  '/register',
+  [
+    check('email', 'Please Enter a Valid Email').isEmail(),
+    check('password', 'Password must be at least 6 characters').isLength({
+      min: 6,
+    }),
+    check('firstName', 'Please Enter Your First Name').not().isEmpty(),
+    check('lastName', 'Please Enter Your Last Name').not().isEmpty(),
+  ],
+  async (req: Request, res: Response): Promise<any> => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    // Hash Password => Using MongDB Middleware
+    try {
+      // Check User Exist
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res.status(400).json({ message: 'User Already Exist' });
+      }
 
-    // Create User & Save User
-    user = new User(req.body);
-    await user.save();
+      // Hash Password => Using MongDB Middleware
 
-    // Create JWT Token
-    const token = await jwt.sign(
-      { _id: user._id },
-      process.env.JWT_SECRET_KEY as string,
-      { expiresIn: '1day' },
-    );
+      // Create User & Save User
+      user = new User(req.body);
+      await user.save();
 
-    // Send JWT Token via Cookie
-    res.cookie('auth_token', token, {
-      httpOnly: true,
-      maxAge: 24 * 60 * 60 * 1000,
-      secure: process.env.NODE_ENV !== 'development',
-    });
+      // Create JWT Token
+      const token = await jwt.sign(
+        { _id: user._id },
+        process.env.JWT_SECRET_KEY as string,
+        { expiresIn: '1day' },
+      );
 
-    // Send Back Response
-    res.status(200).json({ message: 'User Created' });
+      // Send JWT Token via Cookie
+      res.cookie('auth_token', token, {
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000,
+        secure: process.env.NODE_ENV !== 'development',
+      });
 
-    // Catch Errors
-  } catch (error) {
-    console.log(`🚀error on /register =>`, error);
-    res.status(500).json({ message: 'Internal Server Error' });
-  }
-});
+      // Send Back Response
+      res.status(200).json({ message: 'User Created' });
+
+      // Catch Errors
+    } catch (error) {
+      console.log(`🚀error on /register =>`, error);
+      res.status(500).json({ message: 'Internal Server Error' });
+    }
+  },
+);
 
 export default router;
