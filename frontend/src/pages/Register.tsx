867145a5d203f18ebc145ba7784d54cc3c8f5b7e
kiethohoai <kietho.060193@gmail.com>
2024-09-30 13:26:48 +0700

    #15 Testing Toast Component With Register User

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from 'react-hook-form';
 import { useMutation } from 'react-query';
 import * as apiClient from '../api-client';
 import { useAppContext } from '../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
 
 export type RegisterFormData = {
   firstName: string;
@@ -12,6 +13,7 @@ export type RegisterFormData = {
 };
 
 const Register = () => {
+  const navigate = useNavigate();
   const { showToast } = useAppContext();
 
   const {
@@ -29,6 +31,7 @@ const Register = () => {
         message: 'User Registered',
         type: 'SUCCESS',
       });
+      navigate('/');
     },
     onError(error: Error) {
       // console.log(`🚀  error =>`, error.message);
