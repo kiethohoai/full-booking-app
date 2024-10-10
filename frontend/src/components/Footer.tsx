03850e9c7ffd9ca606a6a12d9f3692f3ce16c2ba
kiethohoai <kietho.060193@gmail.com>
2024-10-10 08:34:43 +0700

    55 Fix Footer Text Margin

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <span className="md:text-3xl text-2xl text-white font-bold tracking-tight">
           MernHoliday.Com
         </span>
-        <span className="text-white text-xl font-bold tracking-tight flex flex-col md:flex-row md:text-3xl">
+        <span className="text-white text-xl font-bold tracking-tight flex flex-col md:flex-row md:text-3xl gap-2">
           <p className="cursor-pointer">Privacy Policy</p>
           <p className="cursor-pointer">Term of Services</p>
         </span>
